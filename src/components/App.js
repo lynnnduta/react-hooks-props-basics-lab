e0aa60fd16a5b4ee6c192442fc1df941b8462901
lynn <lynnnduta23@gmail.com>
2024-05-04 12:38:35 +0300

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,9 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin}/>
+      
     </div>
   );
 }
